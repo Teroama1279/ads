0293b0de919cd4e5ebdd66c33eb6d5e73ef8e5bb
Utsu1279 <utsarga75@gmail.com>
2025-01-05 00:30:51 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-const { env } = await import('./src/env.mjs');
+import { env } from './src/env.mjs';
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
@@ -30,6 +20,16 @@ const config = {
     ignoreDuringBuilds: true,
   },
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: false, // Change to true if you want a permanent redirect (301)
+      },
+    ];
+  },
 };
+
 
 export default config;
