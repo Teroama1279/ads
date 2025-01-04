3add9b09d796591999bc5dd497ac494157ece2c4
Utsu1279 <utsarga75@gmail.com>
2025-01-05 00:36:54 +0545

    Update page.tsx

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import EmbedPlayer from '@/components/watch/embed-player';
+'use client';
 
-export const revalidate = 3600;
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Page({ params }: { params: { slug: string } }) {
+  const router = useRouter();
   const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://vidsrc.cc/v2/embed/tv/${id}`} />;
+  const newUrl = `https://456movie.com/tv/watch/${id}`;
+
+  useEffect(() => {
+    // Redirect to the new URL
+    router.push(newUrl);
+  }, [newUrl, router]);
+
+  return <div>Redirecting...</div>; // Show a message while redirecting
 }
