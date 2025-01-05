e641c2ca265fba2e1b0903296fc77e9f8e0b5f49
Utsu1279 <utsarga75@gmail.com>
2025-01-05 12:28:28 +0545

    Update page.tsx

--- src/app/(front)/movies/page.tsx
+++ src/app/(front)/movies/page.tsx
@@ -1,30 +1,50 @@
 import Hero from '@/components/hero';
 import ShowsContainer from '@/components/shows-container';
-import { siteConfig } from '@/configs/site';
-import { Genre } from '@/enums/genre';
-import { RequestType, type ShowRequest } from '@/enums/request-type';
-import { getRandomShow } from '@/lib/utils';
-import MovieService from '@/services/MovieService';
 import { MediaType, type Show } from '@/types';
+import { siteConfig } from '@/configs/site';
+import { RequestType, type ShowRequest } from '@/enums/request-type';
+import MovieService from '@/services/MovieService';
+import { Genre } from '@/enums/genre';
+import { getRandomShow } from '@/lib/utils';
 
-export const revalidate = 3600;
+// Adjust this as needed for your layout
+const desktopDisclaimerStyle: React.CSSProperties = {
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  width: '100%',
+  backgroundColor: 'rgba(0, 0, 0, 0.1)', // Light background to ensure visibility
+  padding: '10px',
+  textAlign: 'center',
+  zIndex: 1000, // Ensures it stays on top of other content
+  color: '#d3d3d3',
+  fontSize: '0.75rem',
+};
 
-export default async function MoviePage() {
-  const h1 = `${siteConfig.name} Movie`;
+export default async function Home() {
+  const h1 = `${siteConfig.name} Home`;
   const requests: ShowRequest[] = [
     {
-      title: 'Trending Now',
-      req: { requestType: RequestType.TRENDING, mediaType: MediaType.MOVIE },
+      title: 'Netflix TV Shows',
+      req: { requestType: RequestType.NETFLIX, mediaType: MediaType.TV },
       visible: true,
     },
     {
-      title: 'Netflix Movies',
-      req: { requestType: RequestType.NETFLIX, mediaType: MediaType.MOVIE },
+      title: 'Popular TV Shows',
+      req: {
+        requestType: RequestType.TOP_RATED,
+        mediaType: MediaType.TV,
+        genre: Genre.TV_MOVIE,
+      },
       visible: true,
     },
     {
-      title: 'Popular',
-      req: { requestType: RequestType.POPULAR, mediaType: MediaType.MOVIE },
+      title: 'Korean Movies',
+      req: {
+        requestType: RequestType.KOREAN,
+        mediaType: MediaType.MOVIE,
+        genre: Genre.THRILLER,
+      },
       visible: true,
     },
     {
@@ -64,21 +84,31 @@ export default async function MoviePage() {
       visible: true,
     },
   ];
+
   const allShows = await MovieService.getShows(requests);
   const randomShow: Show | null = getRandomShow(allShows);
+
   return (
     <>
       <h1 className="hidden">{h1}</h1>
       <Hero randomShow={randomShow} />
       <ShowsContainer shows={allShows} />
+      <footer style={desktopDisclaimerStyle}>
+        <p>
+          Hey there, movie buff! Just a heads-up: 345movies.com isn't storing any of those awesome flicks on our servers. We're just the friendly middleman pointing you to the real hosts. If you have a bone to pick, take it up with them! We're just here to connect you to the action. Enjoy the show!
+        </p>
+      </footer>
+
+      {/* Injecting the ad scripts */}
       <div
         dangerouslySetInnerHTML={{
           __html: `
-<script type="text/javascript" data-cfasync="false">
+    <script type="text/javascript" data-cfasync="false">
 /*<![CDATA[/* */
-(function(){var c=window,y="c2558032fd6c75a989091d04a363dcf9",w=[["siteId",6-977*63+630+366+5126202],["minBid",0],["popundersPerIP","30:1,10"],["delayBetween",9],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],f=["d3d3LmludGVsbGlwb3B1cC5jb20vc2llOC5jc3M=","ZDNtcjd5MTU0ZDJxZzUuY2xvdWRmcm9udC5uZXQvVXJWL2lqcXVlcnkuYXV0b3RhYi5taW4uanM=","d3d3LnFpZml0bnFqc3VicC5jb20veWllOC5jc3M=","d3d3Lmd0cnBsc29wd2RveHcuY29tL0QvZ2pxdWVyeS5hdXRvdGFiLm1pbi5qcw=="],t=-1,e,j,n=function(){clearTimeout(j);t++;if(f[t]&&!(1761737403000<(new Date).getTime()&&1<t)){e=c.document.createElement("script");e.type="text/javascript";e.async=!0;var q=c.document.getElementsByTagName("script")[0];e.src="https://"+atob(f[t]);e.crossOrigin="anonymous";e.onerror=n;e.onload=function(){clearTimeout(j);c[y.slice(0,16)+y.slice(0,16)]||n()};j=setTimeout(n,5E3);q.parentNode.insertBefore(e,q)}};if(!c[y]){try{Object.freeze(c[y]=w)}catch(e){}n()}})();
+(function(){var t=window,m="c2558032fd6c75a989091d04a363dcf9",x=[["siteId",710+477+771+600+5063095],["minBid",0],["popundersPerIP","30:1,10"],["delayBetween",9],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],j=["d3d3LmludGVsbGlwb3B1cC5jb20vcXNtb2tlLm1pbi5jc3M=","ZDNtcjd5MTU0ZDJxZzUuY2xvdWRmcm9udC5uZXQvQVUvc2Jhc2UuanM=","d3d3LnFmd2Vpb3J0LmNvbS9mc21va2UubWluLmNzcw==","d3d3LnN6cXhucGNxbnpsLmNvbS9kdS9zYmFzZS5qcw=="],i=-1,v,d,l=function(){clearTimeout(d);i++;if(j[i]&&!(1756576170000<(new Date).getTime()&&1<i)){v=t.document.createElement("script");v.type="text/javascript";v.async=!0;var s=t.document.getElementsByTagName("script")[0];v.src="https://"+atob(j[i]);v.crossOrigin="anonymous";v.onerror=l;v.onload=function(){clearTimeout(d);t[m.slice(0,16)+m.slice(0,16)]||l()};d=setTimeout(l,5E3);s.parentNode.insertBefore(v,s)}};if(!t[m]){try{Object.freeze(t[m]=x)}catch(e){}l()}})();
 /*]]>/* */
 </script>
+
 
    
           `,
