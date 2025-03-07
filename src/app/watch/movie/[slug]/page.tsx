61c8979a85708ae467cb13107b8e31128f536b08
Utsu1279 <utsarga75@gmail.com>
2025-03-07 22:04:06 +0545

    Update page.tsx

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -4,7 +4,7 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const tmdbID = params.slug.split('-').pop();
-  const newUrl = `https://456movie.com/movie/watch/${tmdbID}`;
+  const newUrl = `https://456movie.net/movie/watch/${tmdbID}`;
 
   // Server-side redirect
   redirect(newUrl);
