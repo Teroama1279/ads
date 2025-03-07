766fcf803643ed7cf825634292ece22d79ffe1a7
Utsu1279 <utsarga75@gmail.com>
2025-03-07 22:04:24 +0545

    Update page.tsx

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 export default function Page({ params }: { params: { slug: string } }) {
   const router = useRouter();
   const id = params.slug.split('-').pop();
-  const newUrl = `https://456movie.com/tv/watch/${id}`;
+  const newUrl = `https://456movie.net/tv/watch/${id}`;
 
   useEffect(() => {
     // Redirect to the new URL
